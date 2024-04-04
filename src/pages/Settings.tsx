fff3afd7bc5b16f6bcc56b0a8a31337d26e3b66e
Teddy <tedloukas12@gmail.com>
2024-04-03 21:40:19 -0400

    initial commit

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { IonPage, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+
+export default function Settings() {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Settings</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+}
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
@@ -13,7 +19,7 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle>Welcome to your page</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
